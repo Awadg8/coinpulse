874ce00112a767f5512610d67580070f35f5895a
Awadhesh Gupta <awadheshg798@gmail.com>
2026-03-31 12:17:52 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Coinpulse",
-  description: "Coinpulse",
+  title: "CoinPulse",
+  description: "Crypto Screener App with a built-in High-Frequency Terminal & Dashboard",
 };
 
 export default function RootLayout({
